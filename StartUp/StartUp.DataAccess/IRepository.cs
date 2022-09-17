82cec70d9bb5391cdb32cd7aa804c397e5f540c3
Paula Olivera <72110690+PaulaOlivera@users.noreply.github.com>
2022-09-17 18:22:47 -0300

    Add DataAccess

---
+++ StartUp/StartUp.DataAccess/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace StartUp.DataAccess
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAllExpression(Expression<Func<T, bool>> expression);
+        T GetOneByExpression(Expression<Func<T, bool>> expression);
+        void InsertOne(T elem);
+        void DeleteOne(T elem);
+        void UpdateOne(T elem);
+        void Save();
+    }
+}
