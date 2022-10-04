9db47d24910781b4df450b0108d1d7d527a88184
belenfurtado <belenfurtadocoria@gmail.com>
2022-10-04 15:51:33 -0300

    Creating test for log in

--- StartUp/StartUp.WebApi.UnitTest/SessionControllerTest.cs
+++ StartUp/StartUp.WebApi.UnitTest/SessionControllerTest.cs
@@ -6,7 +6,9 @@ using StartUp.WebApi.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using IBusinessLogic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Expressions;
 using StartUp.Domain;
 using StartUp.Domain.Entities;
 using StartUp.Domain.SearchCriterias;
@@ -19,13 +21,17 @@ namespace StartUp.WebApi.UnitTest
     public class SessionControllerTest
     {
         private Mock<ISessionService> _serviceMock;
+        private Mock<ITokenAccessService> _tokenServiceMock;
+        private Mock<IUserService> _userServiceMock;
         private SessionController _controller;
 
         [TestInitialize]
         public void Setup()
         {
             _serviceMock = new Mock<ISessionService>(MockBehavior.Strict);
-            _controller = new SessionController(_serviceMock.Object);
+            _tokenServiceMock = new Mock<ITokenAccessService>(MockBehavior.Strict);
+            _userServiceMock = new Mock<IUserService>(MockBehavior.Strict);
+            _controller = new SessionController(_serviceMock.Object, _tokenServiceMock.Object, _userServiceMock.Object);
         }
 
         [TestCleanup]
@@ -38,13 +44,42 @@ namespace StartUp.WebApi.UnitTest
         public void LogInTest()
         {
             var session = new Session();
-            var expectedSession = new SessionDetailModel(session);
-            _serviceMock.Setup(manager => manager.GetSpecificSession(It.IsAny<int>())).Returns(session);
+            var user = new User();
+            user.Id = session.Id;
+            user.Password = session.Password;
+            TokenAccess token = new TokenAccess();
+            token.Token = new Guid();
+            var expectedSession = new SessionModel(session);
+            _serviceMock.Setup(manager => manager.VerifySession(expectedSession)).Returns(user);
+            _serviceMock.Setup(manager => manager.CreateOrRetrieveSession(expectedSession)).Returns(session);
+            _tokenServiceMock.Setup(tService => tService.GetSpecificTokenAccess(session)).Returns(token);
+            _userServiceMock.Setup(uService => uService.SaveToken(user, token.Token.ToString()));
+            
+            var response = _controller.Login(expectedSession);
+            var responseObj = response.Result as CreatedResult;
 
-            var response = _controller.GetSession(session.Id);
-            var okResponseObject = response as OkObjectResult;
+            Assert.AreEqual((int)HttpStatusCode.Created, responseObj.StatusCode);
+        }
+        
+        [TestMethod]
+        public void LogInFailTest()
+        {
+            var session = new Session();
+            var user = new User();
+            user.Id = session.Id;
+            user.Password = session.Password;
+            TokenAccess token = new TokenAccess();
+            token.Token = new Guid();
+            var expectedSession = new SessionModel(session);
+            _serviceMock.Setup(manager => manager.VerifySession(expectedSession)).Returns(user);
+            _serviceMock.Setup(manager => manager.CreateOrRetrieveSession(expectedSession)).Returns(session);
+            _tokenServiceMock.Setup(tService => tService.GetSpecificTokenAccess(session)).Returns(token);
+            _userServiceMock.Setup(uService => uService.SaveToken(user, token.Token.ToString()));
+            
+            var response = _controller.Login(expectedSession);
+            var responseObj = response.Result as CreatedResult;
 
-            Assert.AreEqual(expectedSession, okResponseObject.Value);
+            Assert.AreEqual((int)HttpStatusCode.Created, responseObj.StatusCode);
         }
         
     }
